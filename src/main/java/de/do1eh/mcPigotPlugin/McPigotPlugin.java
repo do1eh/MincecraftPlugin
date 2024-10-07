b1ced731486c9a6ff2ad0ab7cbb35c31a0359229
Ralf Lüsebrink <rnh3@gmx.net>
2024-10-07 21:15:33 +0200

    Jurte

--- src/main/java/de/do1eh/mcPigotPlugin/McPigotPlugin.java
+++ src/main/java/de/do1eh/mcPigotPlugin/McPigotPlugin.java
@@ -13,7 +13,7 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.java.JavaPlugin;
+
 
 
 public final class McPigotPlugin extends JavaPlugin {
